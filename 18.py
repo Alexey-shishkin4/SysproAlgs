60f7a0b5698452a6b596ee5555f9d093b4c26fb6
zxc_ghoul <AlexisV.Shishkin@gmail.com>
2024-05-14 12:48:16 +0700

    fixed 18 task

--- 18.py
+++ 18.py
@@ -7,6 +7,7 @@ def get_priority(op):
         ')': 0,
         '+': 1,
         '-': 1,
+        '**': 2,
         '*': 2,
         '/': 2,
         '%': 2,
@@ -24,33 +25,41 @@ def get_priority(op):
         '&&': 7,
         '||': 8,
         '!': 9,
-        '~': 9
+        '~': 9,
     }
     return priorities[op]
 
 
-def is_operator(token):
-    operators = ['+', '-', '*', '/', '%', '&', '|', '^', '<<', '>>', '<', '<=', '>', '>=', '==', '!=', '&&', '||', '!', '~']
+def is_operator(token):  # << >> <= >= == != && || **
+    operators = ['+', '-', '*', '/', '%', '&', '|', '^', '<<', '>>', '<', '<=', '>', '>=', '==', '!=', '&&', '||', '!', '~', '**']
     return token in operators
 
 
 def infix_to_rpn(expression):
     output = []
     stack = Stack()
-
-    for i in expression:
-        if i.isdigit():
-            output.append(i)
-        elif i == '(':
-            stack.push(i)
-        elif i == ')':
+    flag = False
+    for i in range(len(expression)):
+        if flag:
+            flag = False
+            continue
+        if expression[i].isdigit():
+            output.append(expression[i])
+        elif expression[i] == '(':
+            stack.push(expression[i])
+        elif expression[i] == ')':
             while not stack.is_empty() and stack.peek() != '(':
                 output.append(stack.pop())
             stack.pop()
-        elif is_operator(i):
-            while not stack.is_empty() and get_priority(stack.peek()) >= get_priority(i):
+        elif i + 1 < len(expression) and is_operator(expression[i] + expression[i + 1]):
+            while not stack.is_empty() and get_priority(stack.peek()) > get_priority(expression[i] + expression[i + 1]):
                 output.append(stack.pop())
-            stack.push(i)
+            stack.push(expression[i] + expression[i + 1])
+            flag = True
+        elif is_operator(expression[i]):
+            while not stack.is_empty() and get_priority(stack.peek()) >= get_priority(expression[i]):
+                output.append(stack.pop())
+            stack.push(expression[i])
 
     while not stack.is_empty():
         output.append(stack.pop())
@@ -62,6 +71,7 @@ def infix_to_rpn(expression):
 print(infix_expression)
 print(infix_to_rpn(infix_expression))
 """
+# print(infix_to_rpn("2 ** 3 ** 2"))
 
 
 def test_infix_to_rpn():
@@ -75,6 +85,7 @@ def test_infix_to_rpn():
     assert infix_to_rpn("5 & (3 | 7)") == "5 3 7 | &"
     assert infix_to_rpn("5 | (3 & 7)") == "5 3 7 & |"
     assert infix_to_rpn("!5 | (3 & 7)") == "5 ! 3 7 & |"
+    assert infix_to_rpn("2 ** 3 ** 2") == "2 3 2 ** **"
 
     print("All tests passed successfully!")
 
